a2354f7f39828a4360b1a251521fb0cceb205f29
Carmen Bianca Bakker <carmenbianca@fsfe.org>
2017-10-11 17:13:56 +0200

    Another feature + logging

--- src/reuse/_core.py
+++ src/reuse/_core.py
@@ -22,9 +22,16 @@
 
 """Core functionality of reuse."""
 
+import logging
 import os
 from pathlib import Path
 from typing import Iterator, Union
+
+_logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
+
+
+class License:
+    pass
 
 
 def all_files(directory: Union[Path, str] = None) -> Iterator[Path]:
@@ -41,10 +48,29 @@ def all_files(directory: Union[Path, str] = None) -> Iterator[Path]:
     directory = Path(directory)
 
     for root, dirs, files in os.walk(directory):
+        _logger.debug('currently walking in %s', root)
+
         # Don't walk VCS.
-        if '.git' in dirs:
-            dirs.remove('.git')
+        vcs_dirs = {'.git', '.svn'}
+        intersection = vcs_dirs.intersection(set(dirs))
+        for ignored in intersection:
+            _logger.debug('ignoring %s - VCS', ignored)
+            dirs.remove(ignored)
 
         # TODO: Apply better filtering
         for file_ in files:
+            _logger.debug('yielding %s', file_)
             yield file_
+
+def license_of(path: Union[Path, str]) -> License:
+    """Get the license information of *path*."""
+    path = Path(path)
+    license_path = Path('{}.license'.format(path))
+
+    # TODO: Maybe get license information from central config file if it exists
+    if license_path.exists():
+        # TODO
+        _logger.debug('detected %s', license_path)
+    else:
+        # TODO
+        _logger.debug('searching %s for license information', path)
