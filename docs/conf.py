fad130495af1b9886e85cd117ec6c82a318482b8
Carmen Bianca Bakker <carmenbianca@fsfe.org>
2017-12-08 19:46:22 +0100

    Add typehints from typing

--- docs/conf.py
+++ docs/conf.py
@@ -58,6 +58,7 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
+    'sphinx_autodoc_typehints',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
