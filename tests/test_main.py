03dae8403e956e6c95a6927f2c50456b20b46f10
Carmen Bianca Bakker <carmenbianca@fsfe.org>
2017-12-14 17:18:22 +0100

    Dedent code block

--- tests/test_main.py
+++ tests/test_main.py
@@ -23,9 +23,10 @@
 """Tests for the CLI for reuse."""
 
 from itertools import zip_longest
+from textwrap import dedent
 
 import pytest
-from reuse import _main, _util, __version__
+from reuse import __version__, _main, _util
 
 # pylint: disable=invalid-name
 git = pytest.mark.skipif(
@@ -84,34 +85,35 @@ def test_compile(tiny_repository, runner):
         _main.cli,
         ['compile'])
 
-    expected = """SPDXVersion: SPDX-2.1
-DataLicense: CC0-1.0
-SPDXID: SPDXRef-DOCUMENT
-DocumentName: {dirname}
-DocumentNamespace: http://spdx.org/spdxdocs/spdx-v2.1-04c223f0-4415-47fd-9860-7074a07f753e
-Creator: Person: Anonymous ()
-Creator: Organization: Anonymous ()
-Creator: Tool: reuse-{version}
-Created: 2017-11-08T11:07:30Z
-CreatorComment: <text>This document was created automatically using available reuse information consistent with the REUSE Initiative.</text>
-Relationship: SPDXRef-DOCUMENT describes SPDXRef-8008eeb8d2000e5aa6eaa51b1cdc944d726e1107
-Relationship: SPDXRef-DOCUMENT describes SPDXRef-bb5656f1b5e8283a8e930c54afd9a8bfebe7a548
+    expected = dedent("""\
+        SPDXVersion: SPDX-2.1
+        DataLicense: CC0-1.0
+        SPDXID: SPDXRef-DOCUMENT
+        DocumentName: {dirname}
+        DocumentNamespace: http://spdx.org/spdxdocs/spdx-v2.1-04c223f0-4415-47fd-9860-7074a07f753e
+        Creator: Person: Anonymous ()
+        Creator: Organization: Anonymous ()
+        Creator: Tool: reuse-{version}
+        Created: 2017-11-08T11:07:30Z
+        CreatorComment: <text>This document was created automatically using available reuse information consistent with the REUSE Initiative.</text>
+        Relationship: SPDXRef-DOCUMENT describes SPDXRef-8008eeb8d2000e5aa6eaa51b1cdc944d726e1107
+        Relationship: SPDXRef-DOCUMENT describes SPDXRef-bb5656f1b5e8283a8e930c54afd9a8bfebe7a548
 
-FileName: ./src/code.py
-SPDXID: SPDXRef-8008eeb8d2000e5aa6eaa51b1cdc944d726e1107
-FileChecksum: SHA1: fb17c6dd60b8b8c35128c8a14905e1ef328b1534
-LicenseConcluded: NOASSERTION
-LicenseInfoInFile: GPL-3.0+
-FileCopyrightText: <text>Copyright (C) 2017  Free Software Foundation Europe e.V.</text>
+        FileName: ./src/code.py
+        SPDXID: SPDXRef-8008eeb8d2000e5aa6eaa51b1cdc944d726e1107
+        FileChecksum: SHA1: fb17c6dd60b8b8c35128c8a14905e1ef328b1534
+        LicenseConcluded: NOASSERTION
+        LicenseInfoInFile: GPL-3.0+
+        FileCopyrightText: <text>Copyright (C) 2017  Free Software Foundation Europe e.V.</text>
 
-FileName: ./src/no_license.py
-SPDXID: SPDXRef-bb5656f1b5e8283a8e930c54afd9a8bfebe7a548
-FileChecksum: SHA1: da39a3ee5e6b4b0d3255bfef95601890afd80709
-LicenseConcluded: NOASSERTION
-LicenseInfoInFile: CC0-1.0
-FileCopyrightText: <text>2017 Mary Sue</text>""".format(
-    dirname=tiny_repository.name,
-    version=__version__)
+        FileName: ./src/no_license.py
+        SPDXID: SPDXRef-bb5656f1b5e8283a8e930c54afd9a8bfebe7a548
+        FileChecksum: SHA1: da39a3ee5e6b4b0d3255bfef95601890afd80709
+        LicenseConcluded: NOASSERTION
+        LicenseInfoInFile: CC0-1.0
+        FileCopyrightText: <text>2017 Mary Sue</text>""".format(
+            dirname=tiny_repository.name,
+            version=__version__))
 
     for result_line, expected_line in zip_longest(
             result.output.splitlines(),
