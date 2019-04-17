928d3c4a8e566c0b147020c0df25f996592bed83
Carmen Bianca Bakker <carmen@carmenbianca.eu>
2019-04-17 16:30:17 +0200

    Move the compilation of .mo files into setup.py
    Makefile is now no longer responsible for creating the .mo files.
    
    Incidentally, also moved some plumbing around.

--- setup.py
+++ setup.py
@@ -5,7 +5,10 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import glob
-import os
+import shutil
+import subprocess
+from distutils import cmd
+from distutils.command.build import build
 from pathlib import Path
 
 from setuptools import setup
@@ -16,15 +19,61 @@ test_requirements = ["pytest"]
 
 
 def readme_rst():
+    """Return contents of README.rst"""
     return open("README.rst").read()
 
 
+def changelog_rst():
+    """Return contents of CHANGELOG.rst"""
+    return open("CHANGELOG.rst").read()
+
+
 def mo_files():
-    paths = glob.glob("po/**/**/reuse.mo")
-    return [
-        ("share/locale/{}/LC_MESSAGES".format(Path(path).parts[1]), [path])
-        for path in paths
-    ]
+    """List all .mo files.
+
+    This is a bit of a hack.  The files need to be renamed to "reuse.mo" for
+    gettext to pick up on them.  So they're all moved into individual
+    directories before being included.
+
+    I really wish there were a better, standardised way to include
+    translations, short of including them as package data.
+    """
+    paths = glob.glob("po/*.mo")
+    result = []
+    for path in paths:
+        path = Path(path)
+        lang_dir = Path("po") / path.stem
+        lang_dir.mkdir(exist_ok=True)
+        shutil.copyfile(path, lang_dir / "reuse.mo")
+        result.append(
+            (
+                "share/locale/{}/LC_MESSAGES/".format(path.stem),
+                [str(lang_dir / "reuse.mo")],
+            )
+        )
+    return result
+
+
+class BuildTrans(cmd.Command):
+    """Command for compiling the .mo files."""
+
+    def initialize_options(self):
+        self.po_files = None
+
+    def finalize_options(self):
+        self.po_files = glob.glob("po/*.po")
+
+    def run(self):
+        for po_file in self.po_files:
+            subprocess.run(
+                ["msgfmt", po_file, "-o", po_file.replace(".po", ".mo")]
+            )
+
+
+class Build(build):
+    """Redefined build."""
+
+    sub_commands = [("build_trans", None)] + build.sub_commands
 
 
 if __name__ == "__main__":
@@ -37,10 +86,11 @@ if __name__ == "__main__":
         author_email="carmenbianca@fsfe.org",
         description="reuse is a tool for compliance with the REUSE Initiative "
         "recommendations.",
-        long_description=readme_rst(),
+        long_description=readme_rst() + "\n\n" + changelog_rst(),
         package_dir={"": "src"},
         packages=["reuse"],
         data_files=mo_files(),
+        include_package_data=True,
         entry_points={"console_scripts": ["reuse = reuse._main:main"]},
         install_requires=requirements,
         tests_require=test_requirements,
@@ -54,4 +104,5 @@ if __name__ == "__main__":
             "Programming Language :: Python :: 3.6",
             "Programming Language :: Python :: 3.7",
         ],
+        cmdclass={"build": Build, "build_trans": BuildTrans},
     )
