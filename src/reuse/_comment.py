975391b4993b1ed1af0192d70ddd0638462783ad
Carmen Bianca Bakker <carmenbianca@fsfe.org>
2019-05-15 15:48:06 +0200

    comment: Can now parse Python comments

--- src/reuse/_comment.py
+++ src/reuse/_comment.py
@@ -6,6 +6,12 @@
 headers, in any case.
 """
 
+from textwrap import dedent
+
+
+class CommentParseError(Exception):
+    """An error occurred during the parsing of a comment."""
+
 
 class CommentStyle:
     """Base class for comment style."""
@@ -21,28 +27,77 @@ class CommentStyle:
         multi-line comments.
         """
         text = text.strip()
-        result = ""
+        result = []
         for line in text.splitlines():
-            result += cls.SINGLE_LINE
+            line_result = cls.SINGLE_LINE
             if line:
-                result += " " + line
-            result += "\n"
-        return result.strip()
+                line_result += " " + line
+            result.append(line_result)
+        return "\n".join(result)
 
     @classmethod
     def parse_comment(cls, text: str) -> str:
-        """Uncomment all lines in *text*."""
+        """Uncomment all lines in *text*.
+
+        :raises CommentParseError: if *text* could not be parsed.
+        """
+        text = text.strip()
+        if cls.SINGLE_LINE is not None and text.startswith(cls.SINGLE_LINE):
+            return cls.parse_comment_single(text)
+        if cls.MULTI_LINE[0] is not None and text.startswith(
+            cls.MULTI_LINE[0]
+        ):
+            return cls.parse_comment_multi(text)
+        raise CommentParseError(
+            "Text starts with neither a single- nor multi-line comment"
+        )
+
+    @classmethod
+    def parse_comment_single(cls, text: str) -> str:
+        """Uncomment all lines in *text*, assuming they are commented by
+        single-line comments.
+
+        :raises CommentParseError: if *text* could not be parsed.
+        """
+        text = text.strip()
+        result = []
+        for line in text.splitlines():
+            if not line.startswith(cls.SINGLE_LINE):
+                raise CommentParseError(
+                    "'{}' does not start with a comment marker".format(line)
+                )
+            line = line.lstrip(cls.SINGLE_LINE)
+            result.append(line)
+        result = "\n".join(result)
+        return dedent(result)
+
+    @classmethod
+    def parse_comment_multi(cls, text: str) -> str:
+        """Uncomment all lines in *text*, assuming they are commented by
+        multi-line comments.
+
+        :raises CommentParseError: if *text* could not be parsed.
+        """
+        text = text.strip()
         # TODO
+        raise CommentParseError()
 
 
 class PythonCommentStyle(CommentStyle):
     """Python comment style"""
 
     SINGLE_LINE = "#"
-    MULTI_LINE = (None, None, None)
 
 
 def create_comment(text: str, style: CommentStyle = PythonCommentStyle) -> str:
     """Convenience function that calls :func:`create_comment` of a given style.
     """
     return style.create_comment(text)
+
+
+def parse_comment(
+    text: str, style: CommentParseError = PythonCommentStyle
+) -> str:
+    """Convenience function that calls :func:`parse_comment` of a given style.
+    """
+    return style.parse_comment(text)
--- tests/test_comment.py
+++ tests/test_comment.py
@@ -2,29 +2,55 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-"""All tests for reuse._comment"""
+"""All tests for reuse._comment.
+
+A lot of SPDX tags are explicitly wrong in this module, and replaced with the
+tags REUSE-Copyright and REUSE-License-Identifier. This makes it easier for the
+project itself to remain REUSE compliant.
+"""
 
 from inspect import cleandoc
 
-from reuse._comment import create_comment
+from reuse._comment import create_comment, parse_comment
 
 
 def test_create_comment_python():
     """Create a simple Python comment."""
     text = cleandoc(
         """
-        SPDX-Copyright: Mary Sue
+        REUSE-Copyright: Mary Sue
 
-        SPDX-License-Identifier: GPL-3.0-or-later
+        REUSE-License-Identifier: GPL-3.0-or-later
         """
     )
 
     expected = cleandoc(
         """
-        # SPDX-Copyright: Mary Sue
+        # REUSE-Copyright: Mary Sue
         #
-        # SPDX-License-Identifier: GPL-3.0-or-later
+        # REUSE-License-Identifier: GPL-3.0-or-later
         """
     )
 
     assert create_comment(text) == expected
+
+
+def test_parse_comment_python():
+    """Parse a simple Python comment."""
+    text = cleandoc(
+        """
+        # REUSE-Copyright: Mary Sue
+        #
+        # REUSE-License-Identifier: GPL-3.0-or-later
+        """
+    )
+
+    expected = cleandoc(
+        """
+        REUSE-Copyright: Mary Sue
+
+        REUSE-License-Identifier: GPL-3.0-or-later
+        """
+    )
+
+    assert parse_comment(text) == expected
