1290e35f965c9b2708666dc7a80dee71cd502779
Carmen Bianca Bakker <carmenbianca@fsfe.org>
2019-10-30 12:21:10 +0100

    Add Kirill as contributor

--- src/reuse/header.py
+++ src/reuse/header.py
@@ -1,5 +1,6 @@
 # SPDX-FileCopyrightText: 2019 Free Software Foundation Europe e.V.
 # SPDX-FileCopyrightText: 2019 Stefan Bakker <s.bakker777@gmail.com>
+# SPDX-FileCopyrightText: 2019 Kirill Elagin <kirelagin@gmail.com>
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
