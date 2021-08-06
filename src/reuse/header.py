fsfe/reuse-tool

commit d546b1449aea93ac1bffe677e430e1bc004c8435

Allow creating .license file for write-protected files (#347)

src/reuse/header.py
@@ -522,7 +522,7 @@ def add_arguments(parser) -> None:
         action="store_true",
         help=_("skip files with unrecognised comment styles"),
     )
-    parser.add_argument("path", action="store", nargs="+", type=PathType("r+"))
+    parser.add_argument("path", action="store", nargs="+", type=PathType("r"))
 
 
 def run(args, project: Project, out=sys.stdout) -> int:

tests/test_main_addheader.py
@@ -8,6 +8,7 @@
 
 # pylint: disable=unused-argument
 
+import stat
 from inspect import cleandoc
 
 import pytest
@@ -654,6 +655,45 @@ def test_addheader_explicit_license_unsupported_filetype(
     """
     simple_file = fake_repository / "foo.txt"
     simple_file.write_text("Preserve this")
+
+    result = main(
+        [
+            "addheader",
+            "--license",
+            "GPL-3.0-or-later",
+            "--copyright",
+            "Mary Sue",
+            "--explicit-license",
+            "foo.txt",
+        ],
+        out=stringio,
+    )
+
+    assert result == 0
+    assert (
+        simple_file.with_name(f"{simple_file.name}.license")
+        .read_text()
+        .strip()
+        == cleandoc(
+            """
+            spdx-FileCopyrightText: 2018 Mary Sue
+
+            spdx-License-Identifier: GPL-3.0-or-later
+            """
+        ).replace("spdx", "SPDX")
+    )
+    assert simple_file.read_text() == "Preserve this"
+
+
+def test_addheader_explicit_license_doesnt_write_to_file(
+    fake_repository, stringio, mock_date_today
+):
+    """Adding a header to a .license file if --explicit-license is given,
+    doesn't require write permission to the file, just the directory.
+    """
+    simple_file = fake_repository / "foo.txt"
+    simple_file.write_text("Preserve this")
+    simple_file.chmod(mode=stat.S_IREAD)
 
     result = main(
         [

commit c3c9d2ce6d4400976db6017448041894d8c9f6a8

Verify paths to be writable
To resolve the regression from #347 this code checks if files can
be written and throws an error otherwise.

Signed-off-by: Nico Rikken <nico@nicorikken.eu>

src/reuse/header.py
@@ -15,6 +15,7 @@
 
 import datetime
 import logging
+import os
 import re
 import sys
 from argparse import ArgumentParser
@@ -320,6 +321,16 @@ def _is_uncommentable(path: Path) -> bool:
     return is_uncommentable or is_binary(str(path))
 
 
+def _verify_writable(paths: List[Path], parser: ArgumentParser):
+    for path in paths:
+        if not os.access(path, os.W_OK):
+            parser.error(
+                _(
+                    "'{path}' is not writable, please use --explicit-license"
+                ).format(path=path)
+            )
+
+
 def _verify_paths_line_handling(
     paths: List[Path],
     parser: ArgumentParser,
@@ -553,6 +564,7 @@ def run(args, project: Project, out=sys.stdout) -> int:
 
     # Verify line handling and comment styles before proceeding
     if args.style is None and not args.explicit_license:
+        _verify_writable(paths, args.parser)
         _verify_paths_line_handling(
             paths,
             args.parser,