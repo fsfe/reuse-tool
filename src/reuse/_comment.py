46924aa96cd5b1eea9ce89b9f2361abb694d1043
Florian Snow <florian@familysnow.net>
2022-05-13 12:02:02 +0200

    Remove JsxCommentStyle in favor of using CCommentStyle directly

--- src/reuse/_comment.py
+++ src/reuse/_comment.py
@@ -362,12 +362,6 @@ class JinjaCommentStyle(CommentStyle):
     MULTI_LINE = ("{#", "", "#}")
 
 
-class JsxCommentStyle(CCommentStyle):
-    """JSX comment style."""
-
-    _shorthand = "jsx"
-
-
 class LispCommentStyle(CommentStyle):
     """Lisp comment style."""
 
@@ -525,7 +519,7 @@ EXTENSION_COMMENT_STYLE_MAP = {
     ".jinja2": JinjaCommentStyle,
     ".js": CCommentStyle,
     ".json": UncommentableCommentStyle,
-    ".jsx": JsxCommentStyle,
+    ".jsx": CCommentStyle,
     ".jy": PythonCommentStyle,
     ".ksh": PythonCommentStyle,
     ".kt": CCommentStyle,
@@ -615,7 +609,7 @@ EXTENSION_COMMENT_STYLE_MAP = {
     ".toc": TexCommentStyle,
     ".toml": PythonCommentStyle,
     ".ts": CCommentStyle,
-    ".tsx": JsxCommentStyle,
+    ".tsx": CCommentStyle,
     ".ttl": PythonCommentStyle,  # Turtle/RDF
     ".v": CCommentStyle,  # V-Lang source code
     ".vala": CCommentStyle,
