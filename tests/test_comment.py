dc76a010402f976e774b62b7d0b6459934fbe64b
Carmen Bianca Bakker <carmenbianca@fsfe.org>
2022-09-22 14:32:57 +0200

    Add test for sameline comment parsing
    Signed-off-by: Carmen Bianca Bakker <carmenbianca@fsfe.org>

--- tests/test_comment.py
+++ tests/test_comment.py
@@ -88,6 +88,20 @@ def test_parse_comment_generic_multi(MultiStyle):
     assert MultiStyle.parse_comment(text) == expected
 
 
+def test_parse_comment_sameline_multi(MultiStyle):
+    """If a multi-line comment style is on a single line, it should still be
+    parsed.
+    """
+    text = cleandoc(
+        f"""
+        {MultiStyle.MULTI_LINE.start} Hello {MultiStyle.MULTI_LINE.end}
+        """
+    )
+    expected = "Hello"
+
+    assert MultiStyle.parse_comment(text) == expected
+
+
 def test_base_class_throws_errors():
     """When trying to do much of anything with the base class, expect errors."""
     with pytest.raises(CommentParseError):
