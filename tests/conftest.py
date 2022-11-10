826952f2ecb39b0a39f1b672ef5ca5c81e295010
Pietro Albini <pietro.albini@ferrous-systems.com>
2022-11-10 11:24:42 +0100

    avoid gpg signing commits during tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 # SPDX-FileCopyrightText: 2017 Free Software Foundation Europe e.V. <https://fsfe.org>
 # SPDX-FileCopyrightText: 2022 Florian Snow <florian@familysnow.net>
 # SPDX-FileCopyrightText: 2022 Carmen Bianca Bakker <carmenbianca@fsfe.org>
+# SPDX-FileCopyrightText: 2022 Pietro Albini <pietro.albini@ferrous-systems.com>
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
@@ -169,15 +170,7 @@ def git_repository(fake_repository: Path, git_exe: Optional[str]) -> Path:
     )
 
     subprocess.run([git_exe, "add", str(fake_repository)], check=True)
-    subprocess.run(
-        [
-            git_exe,
-            "commit",
-            "-m",
-            "initial",
-        ],
-        check=True,
-    )
+    git_commit(git_exe, "initial")
 
     return fake_repository
 
@@ -234,15 +227,7 @@ def submodule_repository(
     )
 
     subprocess.run([git_exe, "add", str(submodule)], check=True)
-    subprocess.run(
-        [
-            git_exe,
-            "commit",
-            "-m",
-            "initial",
-        ],
-        check=True,
-    )
+    git_commit(git_exe, "initial")
 
     os.chdir(git_repository)
 
@@ -263,15 +248,7 @@ def submodule_repository(
         ],
         check=True,
     )
-    subprocess.run(
-        [
-            git_exe,
-            "commit",
-            "-m",
-            "add submodule",
-        ],
-        check=True,
-    )
+    git_commit(git_exe, "add submodule")
 
     (git_repository / ".gitmodules.license").write_text(header)
 
@@ -372,6 +349,24 @@ def mock_date_today(monkeypatch):
     date = create_autospec(datetime.date)
     date.today.return_value = datetime.date(2018, 1, 1)
     monkeypatch.setattr(datetime, "date", date)
+
+
+def git_commit(git_exe, message):
+    subprocess.run(
+        [
+            git_exe,
+            # Git can be globally configured to digitally sign all commits,
+            # which causes problems when running the test suite, as the user
+            # would be prompted to authorize the signing multiple times per
+            # test execution. The option below configures git to skip signing.
+            "-c",
+            "commit.gpgSign=false",
+            "commit",
+            "-m",
+            message,
+        ],
+        check=True,
+    )
 
 
 # REUSE-IgnoreEnd
