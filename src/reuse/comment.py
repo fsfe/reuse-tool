0e578fe4cda913f4a0e9bd4d28094036520c72b8
Samuel Gaist <samuel.gaist@idiap.ch>
2022-12-04 17:11:57 +0100

    Add mapping for Qt's .pro file
    This should also cover the proguard-rules.pro file mentionned in
    ticket 614

--- src/reuse/comment.py
+++ src/reuse/comment.py
@@ -621,6 +621,7 @@ EXTENSION_COMMENT_STYLE_MAP = {
     ".pot": PythonCommentStyle,
     ".ppt": UncommentableCommentStyle,
     ".pptx": UncommentableCommentStyle,
+    ".pro": PythonCommentStyle,
     ".proto": CCommentStyle,
     ".ps1": PythonCommentStyle,  # TODO: Multiline comments
     ".psm1": PythonCommentStyle,  # TODO: Multiline comments
