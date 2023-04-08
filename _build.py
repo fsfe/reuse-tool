bc1be1829ce10f5ef0199586e886586c5d97df1d
Carmen Bianca BAKKER <carmenbianca@fsfe.org>
2023-04-08 18:27:07 +0200

    Remove setuptools dependency in _build.py and build time
    Signed-off-by: Carmen Bianca BAKKER <carmenbianca@fsfe.org>

--- _build.py
+++ _build.py
@@ -10,94 +10,74 @@ this stays functional.
 """
 
 import glob
+import logging
 import os
 import shutil
 import subprocess
 from pathlib import Path
-from warnings import warn
 
-from setuptools import Distribution
-from setuptools.command.build_py import build_py
-
-# pylint: disable=attribute-defined-outside-init
-
-
-class Build(build_py):
-    """Redefined build."""
-
-    def initialize_options(self):
-        super().initialize_options()
-        self.po_files = None
-        self.msgfmt = None
-        self.mo_outputs = []
-
-    def finalize_options(self):
-        super().finalize_options()
-        self.po_files = glob.glob("po/*.po")
-        for msgfmt in ["msgfmt", "msgfmt.py", "msgfmt3.py"]:
-            self.msgfmt = shutil.which(msgfmt)
-            if self.msgfmt:
-                break
-
-    def run(self):
-        super().run()
-        if self.msgfmt:
-            for po_file in self.po_files:
-                self.announce(f"compiling {po_file}")
-                lang_dir = str(
-                    Path(self.build_lib)
-                    / "reuse/locale"
-                    / Path(po_file).stem
-                    / "LC_MESSAGES"
-                )
-                destination = str(Path(lang_dir) / "reuse.mo")
-                compile_func = lambda msgfmt, in_file, out: subprocess.run(
-                    [msgfmt, "-o", out, in_file],
-                    check=True,
-                )
-
-                self.mkpath(lang_dir)
-                self.make_file(
-                    po_file,
-                    destination,
-                    compile_func,
-                    (self.msgfmt, po_file, destination),
-                )
-                self.mo_outputs.append(destination)
-
-        else:
-            warn("msgfmt is not installed. Translations will not be included.")
-
-    def get_outputs(self, include_bytecode=1):
-        return (
-            super().get_outputs(include_bytecode=include_bytecode)
-            + self.mo_outputs
-        )
+_LOGGER = logging.getLogger(__name__)
+ROOT_DIR = Path(os.path.dirname(__file__))
+BUILD_DIR = ROOT_DIR / "build"
+PO_DIR = ROOT_DIR / "po"
 
 
-def build():
-    """Main function that runs the compilation."""
-    distribution = Distribution(
-        {
-            "package_dir": {"": "src"},
-        }
-    )
-    cmd = Build(distribution)
-    cmd.inplace = 1
-    cmd.ensure_finalized()
-    cmd.run()
+def mkdir_p(path):
+    """Make directory and its parents."""
+    Path(path).mkdir(parents=True, exist_ok=True)
 
-    # Copy into src/. This appears to be the thing that actually does all the
-    # heavy lifting. I'm not sure why I'm bothering with all the
-    # setuptools-specific logic above.
-    #
-    # In summary: Get .mo files from build directory and put them into
-    # src/reuse/locale/{lang}/LC_MESSAGES/reuse.mo.
-    for output in cmd.get_outputs():
-        relative = Path("src") / os.path.relpath(output, cmd.build_lib)
-        relative.parent.mkdir(parents=True, exist_ok=True)
-        shutil.copyfile(output, relative)
+
+def rm_fr(path):
+    """Force-remove directory."""
+    path = Path(path)
+    if path.exists():
+        shutil.rmtree(path)
+
+
+def main():
+    """Compile .mo files and move them into src directory."""
+    rm_fr(BUILD_DIR)
+    mkdir_p(BUILD_DIR)
+
+    msgfmt = None
+    for executable in ["msgfmt", "msgfmt.py", "msgfmt3.py"]:
+        msgfmt = shutil.which(executable)
+        if msgfmt:
+            break
+
+    if msgfmt:
+        po_files = glob.glob(f"{PO_DIR}/*.po")
+        mo_files = []
+
+        # Compile
+        for po_file in po_files:
+            _LOGGER.info(f"compiling {po_file}")
+            lang_dir = (
+                BUILD_DIR / "reuse/locale" / Path(po_file).stem / "LC_MESSAGES"
+            )
+            mkdir_p(lang_dir)
+            destination = Path(lang_dir) / "reuse.mo"
+            subprocess.run(
+                [
+                    msgfmt,
+                    "-o",
+                    str(destination),
+                    str(po_file),
+                ],
+                check=True,
+            )
+            mo_files.append(destination)
+
+        # Move compiled files into src
+        rm_fr(ROOT_DIR / "src/reuse/locale")
+        for mo_file in mo_files:
+            relative = (
+                ROOT_DIR / Path("src") / os.path.relpath(mo_file, BUILD_DIR)
+            )
+            _LOGGER.info(f"copying {mo_file} to {relative}")
+            mkdir_p(relative.parent)
+            shutil.copyfile(mo_file, relative)
 
 
 if __name__ == "__main__":
-    build()
+    main()
