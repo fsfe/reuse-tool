587ad86785714790a7911cb539b213d76ddcb454
Hugo Osvaldo Barrera <hugo@whynothugo.nl>
2023-07-04 16:29:59 +0200

    Fix mypy complaint about re-using the same variable

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@
 #
 import os
 import sys
-from importlib.metadata import PackageNotFoundError, version
+from importlib.metadata import PackageNotFoundError, version as get_version
 from shutil import copyfile
 
 sys.path.insert(0, os.path.abspath("../src/"))
@@ -73,7 +73,7 @@ author = "Free Software Foundation Europe"
 
 try:
     # The full version, including alpha/beta/rc tags.
-    release = version("reuse")
+    release = get_version("reuse")
 except PackageNotFoundError:
     release = "2.0.0"
 
