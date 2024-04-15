9b350a892fb033bace08fe14fbd8f46681835d1a
sudorook <daemon@nullcodon.com>
2024-04-15 02:08:41 -0500

    Adjust _COPYRIGHT_PATTERNS --merge-copyrights.
    Fixes patterns not matching `spdx-c`, `spdx-symbol`, and
    `string-symbol` correctly, thereby causing merges to not occur.

--- src/reuse/_util.py
+++ src/reuse/_util.py
@@ -114,12 +114,12 @@ _SPDX_TAGS: Dict[str, re.Pattern] = {
 
 _COPYRIGHT_PATTERNS = [
     re.compile(
-        r"(?P<copyright>(?P<prefix>SPDX-(File|Snippet)CopyrightText:)\s+"
+        r"(?P<copyright>(?P<prefix>SPDX-(File|Snippet)CopyrightText:(\s\([cC]\)|\s©)?)\s+"
         r"((?P<year>\d{4} ?- ?\d{4}|\d{4}),?\s+)?"
         r"(?P<statement>.*?))" + _END_PATTERN
     ),
     re.compile(
-        r"(?P<copyright>(?P<prefix>Copyright(\s?\([cC]\))?)\s+"
+        r"(?P<copyright>(?P<prefix>Copyright(\s\([cC]\)|\s©)?)\s+"
         r"((?P<year>\d{4} ?- ?\d{4}|\d{4}),?\s+)?"
         r"(?P<statement>.*?))" + _END_PATTERN
     ),
