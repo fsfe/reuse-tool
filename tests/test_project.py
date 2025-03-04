082aefa63b8b7ee54f19d8b24fcc85b0e8be7dea
Carmen Bianca BAKKER <carmenbianca@fsfe.org>
2025-03-04 22:02:17 +0100

    Move ReuseInfo to copyright
    ReuseInfo will depend on CopyrightNotice, so it makes sense to move it.
    
    Signed-off-by: Carmen Bianca BAKKER <carmenbianca@fsfe.org>

--- tests/test_project.py
+++ tests/test_project.py
@@ -20,7 +20,8 @@ import pytest
 from conftest import RESOURCES_DIRECTORY
 from license_expression import LicenseSymbol
 
-from reuse import _LICENSING, ReuseInfo, SourceType
+from reuse import _LICENSING
+from reuse.copyright import ReuseInfo, SourceType
 from reuse.covered_files import iter_files
 from reuse.exceptions import (
     GlobalLicensingConflictError,
