81a855bbf2ae71b11c772f0bb1d34580fa90dd76
Jan Staněk <jstanek@redhat.com>
2025-07-15 12:21:21 +0200

    Prepare test_cli_main.py for click 8.2
    In click version 8.2.0, when displaying the help because no sub-command
    or option was specified, the exit code changed from 0 to 2 [1].
    
    [1]: https://github.com/pallets/click/pull/1489

--- tests/test_cli_main.py
+++ tests/test_cli_main.py
@@ -19,7 +19,9 @@ class TestMain:
         without_help = CliRunner().invoke(main, [])
         with_help = CliRunner().invoke(main, ["--help"])
         assert without_help.output == with_help.output
-        assert without_help.exit_code == with_help.exit_code == 0
+        # click changed the return value in 8.2
+        assert without_help.exit_code in {0, 2}
+        assert with_help.exit_code == 0
         assert with_help.output.startswith("Usage: reuse")
 
     def test_version(self):
