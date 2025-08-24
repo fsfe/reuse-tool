f7b8fa5d9d8f689990ff7822e4d6f90bc9e49fec
Carmen Bianca BAKKER <carmenbianca@fsfe.org>
2025-08-24 23:00:48 +0200

    Make report compatible with CopyrightNotice

--- src/reuse/report.py
+++ src/reuse/report.py
@@ -698,7 +698,7 @@ class FileReport:  # pylint: disable=too-many-instance-attributes
             "path": PurePath(self.name).as_posix(),
             "copyrights": [
                 {
-                    "value": line,
+                    "value": str(line),
                     "source": reuse_info.source_path,
                     "source_type": (
                         reuse_info.source_type.value
@@ -799,10 +799,13 @@ class FileReport:  # pylint: disable=too-many-instance-attributes
 
         # Copyright text
         report.copyright = "\n".join(
-            sorted(
-                line
-                for reuse_info in reuse_infos
-                for line in reuse_info.copyright_notices
+            map(
+                str,
+                sorted(
+                    line
+                    for reuse_info in reuse_infos
+                    for line in reuse_info.copyright_notices
+                ),
             )
         )
         # Source of licensing and copyright info
